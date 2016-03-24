5dca0511bfa8cbd81b0535ffcefb69acd832c754
Nathalie <Nathalie@172.16.38.209>
2016-03-24 18:42:35 -0400

    added part 2 and misclassification rate

--- Lab3_main.m
+++ Lab3_main.m
@@ -4,7 +4,32 @@ close all;
 
 
 load('feat.mat');
-% aplot(f2);
+%% Part 2
+
+im(1).data = readim(['cloth.im']);
+im(2).data = readim(['cotton.im']);
+im(3).data = readim(['grass.im']);
+im(4).data = readim(['pigskin.im']);
+im(5).data = readim(['wood.im']);
+im(6).data = readim(['cork.im']);
+im(7).data = readim(['paper.im']);
+im(8).data = readim(['stone.im']);
+im(9).data = readim(['raiffa.im']);
+im(10).data = readim(['face.im']);
+
+% for i_n = 1:10
+%     figure(i_n)
+%     imagesc(im(i_n).data)
+%     colormap(gray)
+% end
+
+% % figure(11)
+% % aplot(f2)
+% % figure(12)
+% % aplot(f8)
+% % figure(13)
+% % aplot(f32)
+
 %% Part 3
 % res = 2
 MICDClassifier_1 = MICDClassifier(f2);
@@ -28,6 +53,12 @@ for i = 1:10
     for j = 1:10
         confusion_2(i,j) = histc(classes(i,:),j);
     end
+end
+
+%misclassification rate for each image and for each n
+
+for i = 1 :10
+    misclas(1,i) = 1-(confusion_2(i,i)/16);
 end
 
 % res = 8
@@ -54,6 +85,17 @@ for i = 1:10
         confusion_8(i,j) = histc(classes(i,:),j);
     end
 end
+%misclassification rate for each image and for each n
+% for i = 1 :10
+%     confusion_2(i,11) = sum(confusion_2(i,1:10));
+%     confusion_2(i,12) = sum(confusion_2(i,2:10))/confusion_2(i,11);
+% end
+
+for i = 1 :10
+    misclas(2,i) = 1-(confusion_8(i,i)/16);
+end
+
+
 % res = 32
 
 MICDClassifier_3 = MICDClassifier(f2);
@@ -79,9 +121,23 @@ for i = 1:10
     end
 end
 
+%misclassification rate for each image and for each n
+% for i = 1 :10
+%     confusion_2(i,11) = sum(confusion_2(i,1:10));
+%     confusion_2(i,12) = sum(confusion_2(i,2:10))/confusion_2(i,11);
+% end
+
+for i = 1 :10
+    misclas(3,i) = 1-(confusion_32(i,i)/16);
+end
+
+for i = 1:3
+    misclas(i,11) = mean(misclas(i,1:10));
+end
+
 %% Part 4
 
-% MICDClassifier = MICDClassifier(f8);
+% MICDClassifier = MICDClassifier(f8);??
 % counter = 1;
 % p = 1;
 for i = 1:256
@@ -97,7 +153,10 @@ for i = 1:256
 %     counter = counter +1;
     end
 end
-figure (1)
+
+figure(1)
 imagesc(cimage);
+colormap(gray)
 figure(2)
 imagesc(multim);
+colormap(gray)
